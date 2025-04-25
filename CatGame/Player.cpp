72637ed120e2f3045c5d4c4dcc235b107acb39e8
Rachel Ibarra Channell <channell@usc.edu>
2025-04-24 21:23:01 -0700

    Cat PNG

--- CatGame/Player.cpp
+++ CatGame/Player.cpp
@@ -8,21 +8,21 @@ Player::Player(Game* game)
 : Actor(game)
 {
 	AnimatedSprite* asc = new AnimatedSprite(this, 200);
-	std::vector<SDL_Texture*> idle{GetGame()->GetTexture("Assets/Mario/Idle.png")};
+	std::vector<SDL_Texture*> idle{GetGame()->GetTexture("Assets/Cat/Idle.png")};
 	asc->AddAnimation("idle", idle);
-	std::vector<SDL_Texture*> dead{GetGame()->GetTexture("Assets/Mario/Dead.png")};
+	std::vector<SDL_Texture*> dead{GetGame()->GetTexture("Assets/Cat/Dead.png")};
 	asc->AddAnimation("dead", dead);
-	std::vector<SDL_Texture*> jumpLeft{GetGame()->GetTexture("Assets/Mario/JumpLeft.png")};
+	std::vector<SDL_Texture*> jumpLeft{GetGame()->GetTexture("Assets/Cat/JumpLeft.png")};
 	asc->AddAnimation("jumpLeft", jumpLeft);
-	std::vector<SDL_Texture*> jumpRight{GetGame()->GetTexture("Assets/Mario/JumpRight.png")};
+	std::vector<SDL_Texture*> jumpRight{GetGame()->GetTexture("Assets/Cat/JumpRight.png")};
 	asc->AddAnimation("jumpRight", jumpRight);
-	std::vector<SDL_Texture*> runLeft{GetGame()->GetTexture("Assets/Mario/RunLeft0.png"),
-									  GetGame()->GetTexture("Assets/Mario/RunLeft1.png"),
-									  GetGame()->GetTexture("Assets/Mario/RunLeft2.png")};
+	std::vector<SDL_Texture*> runLeft{GetGame()->GetTexture("Assets/Cat/RunLeft0.png")};
+									  /*GetGame()->GetTexture("Assets/Mario/RunLeft1.png"),
+									  GetGame()->GetTexture("Assets/Mario/RunLeft2.png")};*/
 	asc->AddAnimation("runLeft", runLeft);
-	std::vector<SDL_Texture*> runRight{GetGame()->GetTexture("Assets/Mario/RunRight0.png"),
-									   GetGame()->GetTexture("Assets/Mario/RunRight1.png"),
-									   GetGame()->GetTexture("Assets/Mario/RunRight2.png")};
+	std::vector<SDL_Texture*> runRight{GetGame()->GetTexture("Assets/Cat/RunRight0.png")};
+									   /*GetGame()->GetTexture("Assets/Mario/RunRight1.png"),
+									   GetGame()->GetTexture("Assets/Mario/RunRight2.png")};*/
 	asc->AddAnimation("runRight", runRight);
 	mAnimatedSprite = asc;
 	asc->SetAnimation("idle");
